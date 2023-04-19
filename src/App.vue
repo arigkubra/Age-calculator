f8c4fe270fcc5eab0e6e7257af2ec41e909c05d4
kubra_arig <arigkubra@gmail.com>
2023-04-19 23:26:12 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  
+      <!-- White Card -->
+      <div id="white-card" class="my-auto w-[38rem] h-[28rem] content-center bg-c-white rounded-2xl rounded-br-[20%]">
+        <Input />
+        <Information />
+      </div>
+
+
+</template>
+
+<script setup>
+  import Information from "./components/InformationComp.vue";
+  import Input from "./components/InputComp.vue";
+
+  
+
+
+
+
+
+</script>
+
---
+++ src/components/InformationComp.vue
@@ -0,0 +1,29 @@
+<template>
+    <!-- Information -->
+    <div class="flex flex-col space-y-3 ml-14 text-6xl mt-14">
+
+        <!-- Item 1 -->
+        <div class="">
+            <i class="font-extrabold text-c-purple">- - </i>
+            <i class="font-extrabold">years</i>
+        </div>
+
+        <!-- Item 2 -->
+        <div>
+            <i class="font-extrabold text-c-purple">- - </i>
+            <i class="font-extrabold">months</i>
+        </div>
+
+        <!-- Item 3 -->
+        <div>
+            <i class="font-extrabold text-c-purple">- - </i>
+            <i class="font-extrabold">days</i>
+        </div>
+
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
